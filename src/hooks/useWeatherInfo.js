5da39baa926138587b61f5207caf8af1dbe952a4
Carlos Agundez <internetcharlytech@gmail.com>
2023-02-23 18:17:55 +0100

    Avoid previous search

--- src/hooks/useWeatherInfo.js
+++ src/hooks/useWeatherInfo.js
@@ -1,16 +1,19 @@
 /* eslint-disable dot-notation */
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchWeather } from '../data/weatherInfo'
 
 export function useWeatherInfo({ search }) {
 	const [infoWeather, setInfoWeather] = useState({})
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState(null)
+	const previousSearch = useRef(search)
 
 	const getWeatherInfo = async () => {
+		if (search === previousSearch.current) return
 		try {
 			setLoading(true)
 			setError(null)
+			previousSearch.current = search
 			const newWeatherInfo = await searchWeather({ search })
 			setInfoWeather(newWeatherInfo)
 		} catch (e) {
